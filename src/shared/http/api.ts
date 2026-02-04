31c827b7f427deb5196979cf350c48b996151315
Matheus Fellipe <matheus060399@gmail.com>
2026-02-04 13:34:24 -0400

    infra/add axios, .env e alterando gitignore

---
+++ src/shared/http/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import type { AxiosInstance } from "axios";
+
+const apiClient: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+export default apiClient;
